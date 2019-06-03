656646f41b0632619054a1a16eab952c840b26ad
Cas van Rijbroek <Casvanrijbroek@hotmail.com>
2019-06-03 22:26:21 +0200

    documentation updated, command line pipeline added

--- textmining.py
+++ textmining.py
@@ -1,14 +1,20 @@
 from nltk.corpus import stopwords
+from pymongo import MongoClient
 from itertools import product
 from Bio import Entrez
 import nltk
+import sys
 import re
 
-Entrez.email = "casvanrijbroek@hotmail.com"
+""" This script can be used to perform textmining on PubMed articles based on a set of search terms. The search terms 
+need to be given in multiple categories (nested lists), because the script only compares keywords of different 
+categories with each other. To use this script, simply import it into your application and pass the nested list of
+search terms to the main function (other functions can be called independently to fit a different pipeline too). 
+"""
 
 
-def main(search_words):
-    """ This is the main function that calls the other functions required to text mine.
+def textmine(search_words):
+    """ This function can be called from another script to perform textmining on search words.
 
     :param search_words: A nested list of search terms, each list should contain words that are related to each
     other and therefor need not be compared (such as terms related to a single disease).
@@ -16,10 +22,89 @@ def main(search_words):
     article found some general information is stored together with the assigned co-occurrence score.
     """
     products = create_products(search_words)
+
+    return main(products)
+
+
+def textmine_new_term(new_term, upper_term):
+    """ This function is called when the script is called from the command line. The intent behind this function is to
+    add new terms to the existing pool of results in the database.
+
+    :param new_term: A string containing a new search word.
+    :param upper_term: A string containing the upper term of the search word or "new" if the word is part of a new
+    category.
+    :return: A list of dictionaries that contain the found co-occurrence between the given search terms. For each
+    article found some general information is stored together with the assigned co-occurrence score.
+    """
+    search_words = get_search_words(new_term, upper_term)
+    products = create_products_additional_term(search_words)
+
+    return main(products)
+
+
+def main(products):
+    """ This is the main function that calls the other functions required to perform textmining on products.
+
+    :param products: A list with sets that contain every wanted combination of search words.
+    :return: A list of dictionaries that contain the found co-occurrence between the given search terms. For each
+    article found some general information is stored together with the assigned co-occurrence score.
+    """
+    Entrez.email = "casvanrijbroek@hotmail.com"
     queries = create_queries(products)
+    print(queries)
     output = mine(list(products), queries)
+    print(output)
 
     return output
+
+
+def get_search_words(new_term, upper_term):
+    """ This function fetches all existing search terms from the database with the exclusion of the ones contained
+    in the same category as the new term. The new term is also included in the output.
+
+    :param new_term: A string containing a new search word.
+    :param upper_term: A string containing the upper term of the search word or "new" if the word is part of a new
+    category.
+    :return: A nested list of search terms, each list should contain words that are related to each
+    other and therefor need not be compared (such as terms related to a single disease).
+    """
+    search_words = []
+
+    client = MongoClient("mongodb+srv://textminingdata-m49re.azure.mongodb.net/test", 27017,
+                         username="admin", password="blaat1234")
+    db = client.gourd_goru
+    collection = db.searchWords
+
+    if upper_term == "new":
+        terms = collection.find({})
+        search_words.append([new_term])
+    else:
+        terms = collection.find({"upper_term": {"$ne": upper_term}})
+        search_words.append([new_term])
+
+    for category in terms:
+        search_words.append(category["sub_terms"])
+
+    return search_words
+
+
+def create_products_additional_term(search_words):
+    """ This function creates all wanted combinations of search words based on the addition of a new search word into
+    the database. This means that the new search word will be combined with all other search words from different
+    categories.
+
+    :param search_words: A nested list of search terms, each list should contain words that are related to each
+    other and therefor need not be compared (such as terms related to a single disease).
+    :return: A list with sets that contain every wanted combination of search words.
+    """
+    products = set()
+
+    for category in search_words:
+        if category != search_words[0]:
+            for word in category:
+                products.add((word, search_words[0][0]))
+
+    return products
 
 
 def create_products(search_words):
@@ -214,3 +299,28 @@ def format_output(article, score):
              "journal": medline["Article"]["Journal"]["Title"],
              "score": score,
              "pub_date": pub_date})
+
+
+def save_results(results):
+    """ This function saves the textmining results in the database.
+    (NOTE: this function is NOT automatically called by the others functions. To save results this function needs to be
+    specifically called by the user after textmining. The command line pipeline DOES automatically call this function.)
+
+    :param results: A list of dictionaries that contain the found co-occurrence between the given search terms. For each
+    article found some general information is stored together with the assigned co-occurrence score.
+    """
+    client = MongoClient("mongodb+srv://textminingdata-m49re.azure.mongodb.net/test", 27017,
+                         username="admin", password="blaat1234")
+
+    db = client.gourd_goru
+    collection = db.miningResults
+
+    collection.insert_many(results)
+
+
+if __name__ == "__main__":
+    """ When the script is called from the command line. This code ensures the new term is going to be textmined and
+    added to the database.
+    """
+    textmining_results = textmine_new_term(sys.argv[1], sys.argv[2])
+    save_results(textmining_results)
